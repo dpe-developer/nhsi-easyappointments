a9a4da601993b0c9db7c300a0ba5f45ec5b0ed41
David <dpe.developer001@gmail.com>
2023-12-03 19:18:50 +0800

    Update appointment_details.php

--- application/views/emails/appointment_details.php
+++ application/views/emails/appointment_details.php
@@ -11,7 +11,7 @@
     </div>
 
     <div id="content" style="padding: 10px 15px;">
-    <h2><?= $email_title ?></h2>
+        <h2><?= $email_title ?></h2>
         <h3><?= $email_message ?></h3>
         <br>
         <br>
@@ -37,7 +37,7 @@
             <strong>REMINDERS:</strong>
             <br>
             <ol>
-                <li>You are required to wear <strong>SURGICAL MASK and CLEAR FULL FACE SHIELD within the clinic’s premises.</strong></li>
+                <li>You are required to wear <strong>SURGICAL MASK and CLEAR FULL FACE SHIELD within the clinic's premises.</strong></li>
                 <li>Please bring black ball pen, your <strong>VALID ORIGINAL PASSPORT</strong> and (referral letter for Australia visa applicants only)</li>
                 <li><strong>To facilitate processing,</strong> please print and fill out the NHSBI form, COVID Health Checklist (for ALL visa applicants) and HIV DOH Form and consent form (15 years old and above visa applicants) from our website <a href="https://nhsgroup.ph/baguio.html">nhsgroup.ph/baguio.html</a> <strong>BEFORE</strong> your appointment date.</li>
                 <li><strong>Please print the appointment confirmation letter that will be sent to you after you have replied with this email with your scanned deposit slip and present it upon your arrival at the clinic.</strong></li>
@@ -108,39 +108,6 @@
                 <td style="padding: 3px;"><?= $customer_notes ?></td>
             </tr>
         </table>
-        <!-- Custom info -->
-        <h2>Please read carefully</h2>
-        <p>
-            Dear <?= $customer_name ?>, 
-            <br>
-            <br>
-            Your appointment schedule is in PHILIPPINE TIME.<br>The 'Start' time shown above is shown in YOUR LOCAL TIMEZONE (Please convert the 'Start' time shown above to PHILIPPINE TIMEZONE to view your appointment schedule in PHILIPPINE TIME and avoid confusion).
-            <br>
-            <br>
-             This email confirms your Immigration Medical Examination appointment at Nationwide Health Systems Aux, Inc Makati. <strong>Please be in the clinic 30 minutes before your scheduled time </strong>and proceed to Room 24-A (second floor near the elevator) for initial screening and registration. If for any reason you cannot make it to your appointment, please let us know within 48 hours of your appointment time.
-        </p>
-        <br>
-        <br>
-
-        <strong>*Please note: If you are more than 30 minutes late for your scheduled appointment time, you will be given the option of either being seen that day as a walk-in, if the schedule permits, or rescheduled for a later date.</strong>
-
-        <br>
-        <br>
-
-        <strong>REMINDERS:</strong>
-        <br>
-        <ol>
-            <li>You are required to wear SURGICAL MASK within the clinic's premises.</li>
-            <li>Please bring your <strong>VALID ORIGINAL PASSPORT</strong> (for Canada, Australia and New Zealand applicants) and referral letter for Australia visa applicants only</li>
-            <li>Print and fill out the HIV DOH Form and consent form (15 year old and above PERMANENT visa applicants) at <a href="https://nhsiphilippines.com/makaticity/">https://nhsiphilippines.com/makaticity/</a> <strong>BEFORE</strong> your appointment date.</li>
-            <li><strong>Please proceed to room 24-A (screening room) for registration.</strong></li>
-            <li>Kindly bring black or blue ball pen.</li>
-            <li>We only accept cash payments. For the fees, please visit our website at <a href="https://nhsiphilippines.com/makaticity/">https://nhsiphilippines.com/makaticity/</a></li>
-            <li>Laboratory tests are NON- FASTING.</li>
-            <li>For New Zealand applicants: Females within the reproductive age group are advised not to schedule their medical examination during their menstrual period. Otherwise, you will be required to come back one week after the last day of their menstruation.</li>
-            <li>Companions will not be allowed inside the clinic, and we therefore strongly discourage clients from bringing one. (For elderly and PWDs who will need to be accompanied, please inform us beforehand so arrangements can be made.)</li>
-        </ol>
-        <!-- END OF Custom info -->
     </div>
 
     <div id="footer" style="padding: 10px; text-align: center; margin-top: 10px;
